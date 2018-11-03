aab6223caba36ed779435ecd93cc6649ba86427d
lachim kabucz <czubakmichal@gmail.com>
2018-11-03 13:03:05 +0100

    Commit 4 tests

---
+++ src/test/java/pl/sdacademy/vending/util/ConfigurationTest.java
@@ -0,0 +1,63 @@
+package pl.sdacademy.vending.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ConfigurationTest {
+    private Configuration testedConfig;
+
+    @Before
+    public void init(){
+        testedConfig = new Configuration();
+    }
+
+    @Test
+    public void shouldReturnDefaultStringValueWhenPropertyIsUnknown() {
+        // Given
+        String unknownPropertyName = "iwquuwuuuwooownnjkw";
+        String expectedDefault = "javaIsAwesome";
+        // When
+        String propertyValue = testedConfig.getStringProperty(unknownPropertyName, expectedDefault);
+        // Then
+        assertEquals(expectedDefault, propertyValue);
+    }
+
+
+    @Test
+    public void shouldReturnDefaultNumericalValueWhenPropertyIsUnknown() {
+        // Given
+        String unknownPropertyValue = "uuuuuuussssssss";
+        Long expectedLongDefault = 6L;
+        // When
+        Long propertyValue = testedConfig.getLongProperty(unknownPropertyValue, expectedLongDefault);
+        // Then
+        assertEquals(expectedLongDefault, propertyValue);
+    }
+
+    @Test
+    public void shouldReturnExistingStringValue() {
+        // Given
+        String propertyName = "test.property.string";
+        String expectedStringValue = "qwerty";
+        // When
+        String propertyValue = testedConfig.getStringProperty(propertyName, "unknow");
+        // Then
+        assertEquals(expectedStringValue, propertyValue);
+    }
+    @Test
+    public void shouldReturnExistingLongValue() {
+        // Given
+        String propertyName = "test.property.long";
+        Long expectedLongValue = 9L;
+        // When
+        Long propertyValue = testedConfig.getLongProperty(propertyName, 13L);
+        // Then
+        assertEquals(expectedLongValue, propertyValue);
+    }
+}
+
+
+
